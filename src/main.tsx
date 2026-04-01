1081e49a14dc929816f5afa11e8276cc6ca6fc77
Dayanand MG <mgdayanand@gmail.com>
2026-04-01 20:26:53 +0530

    added main css files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./app/router";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        console.log('Service Worker registered successfully with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed: ', error);
+      });
+  });
+}
