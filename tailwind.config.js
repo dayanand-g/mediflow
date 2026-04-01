934a311ecb27a9f31a47a85276101f4d768b90db
Dayanand MG <mgdayanand@gmail.com>
2026-04-01 19:56:49 +0530

    secondary project configuration and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class"],
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -7,11 +7,43 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#2563eb", // blue
-        secondary: "#10b981", // green
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+        },
+      },
+
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
-
